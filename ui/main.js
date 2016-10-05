a77a6b8a65a2b48e467de8422db4c86596f9232a
Mukesh Sahu <mukeshsahu0810@gmail.com>
2016-10-06 00:34:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,19 +1,23 @@
-console.log('Loaded!');
-//Change the text of main text div
+// Counter code
+var button = document.getElementById('counter');
 
-var element = document.getElementById('main-text');
+button.onclick = function () {
+  //Create a request object
+  var request = new XMLHttpRequest();
+   //Capture the response and store it in a variable 
+ request.onreadystatechange = function () {
+      if (request.readyState === XMLHttpRequest.DONE) {
+          //Take some action
+          if(request.status === 200) {
+          var counter = request.responseText;
+          var span = document.getElementById('count');
+          span.innerHTML = counter.toString();
+      }
+    }
+    //Not done yet
+  }
 
-element.innerHTML = 'New value';
-
-//Move the image
-
-var img = document.getElementById('madi');
-var marginLeft=0;
-function moveRight(){
-    marginLeft=marginLeft + 1;
-    img.style.marginLeft = marginLeft + 'px';
-}
-
-img.onclick = function() {
-   var interval =setInterval(moveRight, 50);
+//make the request
+request.open('GET','http://http://mukesh1729.imad.hasura-app.io/counter',true);
+request.send(null);
 };
