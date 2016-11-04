a89ebd72fe563b087f35a097c3beffa0ddba02ec
Mukesh Sahu <mukeshsahu0810@gmail.com>
2016-11-04 09:53:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,19 +2,7 @@
 var button = document.getElementById('counter');
 
 button.onclick = function () {
-  //Create a request object
-  var request = new XMLHttpRequest();
-   //Capture the response and store it in a variable 
- request.onreadystatechange = function () {
-      if (request.readyState === XMLHttpRequest.DONE) {
-          //Take some action
-          if(request.status === 200) {
-          var counter = request.responseText;
-          var span = document.getElementById('count');
-          span.innerHTML = counter.toString();
-      }
-    }
-    //Not done yet
+  
   }
 
 //make the request
@@ -27,14 +15,28 @@ var nameInput=document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
-    //Make arequest to the server and send the name
     
-    //Capture a list of names and render it as a list
-    var names = ['name1','name2','name3','name4'];
-    var list = '';
-    for(var i=0;i<names.length;i++){
-        list+='<li>' + names[i] + '</li>';
+    //Create a request object
+  var request = new XMLHttpRequest();
+   //Capture the response and store it in a variable 
+ request.onreadystatechange = function () {
+      if (request.readyState === XMLHttpRequest.DONE) {
+          //Take some action
+          if(request.status === 200) {
+              //Capture a list of names and render it as a list
+                 var names = ['name1','name2','name3','name4'];
+                var list = '';
+                for(var i=0;i<names.length;i++){
+                    list+='<li>' + names[i] + '</li>';
+                }
+                var ul=document.getElementById('namelist');
+                ul.innerHtml=list;
+          
+      }
     }
-    var ul=document.getElementById('namelist');
-    ul.innerHtml=list;
+    //Not done yet
+};
+
+request.open('GET','http://mukesh1729.imad.hasura-app.io/submit-name?name=' + name,true);
+request.send(null);
 };
