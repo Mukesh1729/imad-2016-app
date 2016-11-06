85c644ae4cbe9fbffcd73144af784ff387572cb3
Mukesh Sahu <mukeshsahu0810@gmail.com>
2016-11-06 11:14:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,8 +2,26 @@
 var button = document.getElementById('counter');
 
 button.onclick = function () {
+  // Create a request object
+  var request=new XmlHttpRequest();
   
-  }
+  //Capture the response and store in a new variable
+   request.onreadystatechange = function () {
+      if (request.readyState === XMLHttpRequest.DONE) {
+          //Take some action
+          if(request.status === 200) {
+              //Capture a list of names and render it as a list
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHtml = counter.toString();
+          }
+      }
+      //Not done yet
+  };
+  //Make the request
+  request.open('GET','http://mukesh1729.imad.hasura-app.io/counter',true);
+  request.send(null);
+};
 
 //Submit name
 var nameInput=document.getElementById('name');
@@ -14,7 +32,7 @@ submit.onclick = function(){
     //Create a request object
   var request = new XMLHttpRequest();
    //Capture the response and store it in a variable 
- request.onreadystatechange = function () {
+  request.onreadystatechange = function () {
       if (request.readyState === XMLHttpRequest.DONE) {
           //Take some action
           if(request.status === 200) {
