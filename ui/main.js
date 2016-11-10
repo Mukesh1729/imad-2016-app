402e9df012d6afc1b8092626171717714198ebe1
Mukesh Sahu <mukeshsahu0810@gmail.com>
2016-11-11 02:37:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -83,7 +83,7 @@ function loadLoginForm () {
 function loadLoggedInUser (username) {
     var loginArea = document.getElementById('login_area');
     loginArea.innerHTML = `
-        <h3> Hi <i>${username}</i></h3>
+        <h3> Hi <i>${username}</i> Welcome to You this Awesome Blogs</h3>
         <a href="/logout">Logout</a>
     `;
 }
